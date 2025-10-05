1c447833e5025e713f1cf2136b09469a4335187f
nbala123 <111918527+nbala123@users.noreply.github.com>
2025-10-04 20:25:51 -0500

    lv1 progress committ

---
+++ scripts/player_platform.gd
@@ -0,0 +1,40 @@
+extends CharacterBody2D
+
+@export var init_pos : Vector2
+ 
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+var double_jump = true
+
+func player(): #identifier class
+	pass
+
+func _ready() -> void:
+	position = init_pos
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	else:
+		double_jump = true
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		elif double_jump:
+			velocity.y=JUMP_VELOCITY
+			double_jump = false
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
